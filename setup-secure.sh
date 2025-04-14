27de43fd570d1a8b4f629f9c451f4d37174e2f28
Jinchueiki <118166123+Jinchueiki@users.noreply.github.com>
2025-04-14 10:26:45 +0200

    Update setup-secure.sh

--- setup-secure.sh
+++ setup-secure.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-echo "🔄 Mise à jour de la VM..."
+echo "Mise à jour de la VM..."
 sudo apt update && sudo apt upgrade -y
 
-echo "🧱 Installation de UFW (pare-feu)..."
+# 2. Configurer le fuseau horaire (France)
+echo "[+] Configuration du fuseau horaire sur Europe/Paris..."
+sudo timedatectl set-timezone Europe/Paris
+echo "[+] Fuseau horaire actuel : $(timedatectl | grep 'Time zone')"
+
+echo "Installation de UFW (pare-feu)..."
 sudo apt install -y ufw
 sudo ufw allow OpenSSH
 sudo ufw enable
 
-echo "🔐 Installation de fail2ban..."
+echo "Installation de fail2ban..."
 sudo apt install -y fail2ban
 sudo systemctl enable fail2ban
 sudo systemctl start fail2ban
 
-echo "✅ Sécurisation terminée."
+echo "Sécurisation terminée."
